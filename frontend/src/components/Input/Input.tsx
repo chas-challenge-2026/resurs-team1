1981fc1192d3582b9c8f03e8fa1e22df7302d4e9
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-08-31 14:57:17 +0200

    #53 add styling to input component

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,33 +1,40 @@
 import type { InputHTMLAttributes } from "react";
+import { RiErrorWarningLine } from "react-icons/ri";
 import s from "./Input.module.css"
 
 interface BaseInputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: string
+  information?: string
 }
 
 interface StandardInputProps extends BaseInputProps {
   id: string;
-  label: string
+  label: string;
 }
 
 interface SearchInputProps extends BaseInputProps {
   type: "search";
   "aria-label": string;
   id?: never;
-  label?: never
+  label?: never;
 }
 
 type InputProps = StandardInputProps | SearchInputProps
 
 const Input = (props: InputProps) => {
-  const { type="text", label, error, className, id, ...rest } = props
+  const { type="text", label, error, information, className, id, ...rest } = props
 
-  const combinedClassName = [s.input, className].filter(Boolean).join(" ")
+  const combinedClassName = [s.input, "input-base", className, error && s.errorBorder].filter(Boolean).join(" ")
+
+  //Id for screen reader
+  const errorId = id && error ? `${id}-error` : undefined
+  const infoId = id && information ? `${id}-info` : undefined
+  const describedBy = errorId || infoId
 
   return(
     <div className={s.wrapper}>
       {label && (
-        <label htmlFor={id} className={s.label}>
+        <label htmlFor={id}>
           {label}
         </label>
       )}
@@ -36,10 +43,21 @@ const Input = (props: InputProps) => {
         id={id}
         type={type}
         className={combinedClassName}
+        aria-invalid={!!error}
+        aria-describedby={describedBy}
         {...rest}
       />
 
-      {error && <span className={s.errorMessage}>{error}</span>}
+      {information && !error && 
+        <span id={infoId} className={s.information}>{information}</span>
+      }
+
+      {error && 
+        <span id={errorId} className={s.error}>
+          <span>{<RiErrorWarningLine aria-hidden={true}/>} </span>
+          <span>{error}</span>
+        </span>
+      }
 
     </div>
   )
