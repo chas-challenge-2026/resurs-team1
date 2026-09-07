7d8e236b5df7eb739fdcefd95d145ec42b716c84
Joso1207 <joso1207@student.miun.se>
2026-09-07 15:14:24 +0200

    Missing Repository Annotation

--- backend/ResursPortal/src/main/java/se/comerit/resurs/persistence/CaseWorkerRepository.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/persistence/CaseWorkerRepository.java
@@ -1,10 +1,12 @@
 package se.comerit.resurs.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.comerit.resurs.persistence.model.CaseWorker;
 
 import java.util.Optional;
 
+@Repository
 public interface CaseWorkerRepository extends JpaRepository<CaseWorker, Long> {
     Optional<CaseWorker> findByEmail(String email);
 }
